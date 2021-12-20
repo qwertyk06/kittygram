46a6055e8acaa562fa5ed1bee6f642ab173b12cc
qwertyk06 <qwertyk06@yandex.ru>
2021-12-20 03:24:28 +0300

    4 вариант, нерабочий (урл-сы старые)

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,29 @@
+from rest_framework import viewsets 
+
+from .models import Cat
+from .serializers import CatSerializer
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
+# from rest_framework import generics
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
 # from rest_framework import status
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
@@ -5,11 +31,6 @@
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
 # from rest_framework import status
-
-from rest_framework import generics
-
-from .models import Cat
-from .serializers import CatSerializer
 
 
 # class APICat(APIView):
@@ -25,14 +46,6 @@ from .serializers import CatSerializer
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class CatList(generics.ListCreateAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
-
-
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
 
 
 
